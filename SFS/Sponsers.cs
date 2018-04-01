8b472b71fbdb8e47490b49b16ef61cd5305a6e9f
Aya <34710060+AyaEissa@users.noreply.github.com>
2018-04-01 19:43:04 +0200

    Sponsers class

---
+++ SFS/Sponsers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFS
+{
+    class Sponsers
+    {
+        string Name;
+        int Contract_date;
+        string Sponser_name;
+        string Sponser_Number;
+
+        public Sponsers()
+        {
+            Name = "";
+            Contract_date = 0;
+            Sponser_name = "";
+            Sponser_Number = "";
+        }
+        public Sponsers(string a,int b,string c,string d)
+        {
+            Name = a;
+            Contract_date = b;
+            Sponser_name = c;
+            Sponser_Number = d;
+
+        }
+        public void setName(string Name)
+        {
+            this.Name = Name;
+        }
+        public string GetName()
+        {
+            return this.Name;
+
+        }
+
+        public void SetContract_date(int Contract_date)
+        {
+            this.Contract_date = Contract_date;
+        }
+        public int GetContract_date()
+        {
+            return this.Contract_date ;
+        }
+        public void SetSponser_name(string Sponser_name)
+        {
+            this.Sponser_name = Sponser_name;
+
+        }
+        public string GetSponser_name()
+        {
+          return  this.Sponser_name;
+        }
+        public void SetSponser_Number(string Sponser_Number)
+        {
+            this.Sponser_Number = Sponser_Number;
+        }
+        public string GetSponser_Number(string Sponser_Number)
+        {
+           return this.Sponser_Number;
+        }
+
+    }
+}
