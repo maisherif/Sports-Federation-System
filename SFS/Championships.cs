451be2780699464bdebb4d4bee3dc1d1bd471e15
Aya <34710060+AyaEissa@users.noreply.github.com>
2018-04-02 17:04:56 +0200

    championship class (#12)
    * Delete .DS_Store
    
    * championship class

---
+++ SFS/Championships.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFS
+{
+    class Championships
+    {
+        string place;
+        List<Team> teamList;
+       // List<Results> teamResults;
+        public Championships()
+        {
+            place = "";
+            teamList = new List<Team>();
+           // teamResults = new List<Results>();
+           
+        }
+        public Championships(string placee)
+        {
+            place = placee;
+        }
+        public void setPlace(string place)
+        {
+            this.place = place;
+        }
+        public string GetPlace()
+        {
+            return this.place;
+        }
+
+    }
+}
